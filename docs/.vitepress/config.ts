9cc5aec4bca958141865943f2ea265bcfbf50d49
Jacek Galanciak <jacek.galanciak@gmail.com>
2026-03-07 21:02:03 +0100

    Try ayu-dark in docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -230,7 +230,7 @@ export default defineConfig({
   markdown: {
     theme: {
       light: 'catppuccin-latte',
-      dark: 'catppuccin-mocha'
+      dark: 'ayu-dark'
     }
   },
   themeConfig: {
